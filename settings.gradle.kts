66744a090c7b17662275683ba22bdc50977de3b2
Bahadır YILDIZ <bahadir@bahadiryildiz.net>
2025-08-16 22:46:58 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,27 @@
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+pluginManagement {
     repositories {
+        // AGP burada -> ŞART
         google()
+        // Kotlin plugin vb.
         mavenCentral()
+        // Gradle plugin portalı
+        gradlePluginPortal()
+        // JitPack (AndroidTvRemote için)
         maven(url = "https://jitpack.io")
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // Android & Jetpack
+        google()
+        // JmDNS, slf4j-android vb.
+        mavenCentral()
+        // JitPack (AndroidTvRemote)
+        maven(url = "https://jitpack.io")
+    }
+}
+
+rootProject.name = "MiStickRemote"
+include(":app")
